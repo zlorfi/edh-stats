e548837199620ac437bb5b6f77e72105b45ad3b9
Michael Skrynski <zlorfi@gmail.com>
2026-01-17 20:25:53 +0100

    Remove migration docs and init scripts

--- backend/init-db/01-init.sql
+++
@@ -1,8 +0,0 @@
--- PostgreSQL initialization script
--- This runs automatically when PostgreSQL starts as the postgres superuser
-
--- NOTE: The database 'edh_stats' is created automatically by the POSTGRES_DB environment variable
--- in docker-compose.yml, so we don't need to create it here.
--- This file can be used for any additional initialization that should happen
--- before the application connects, but it is currently a no-op since the
--- schema is created by the database.js migrations runner.
